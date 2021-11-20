005949a361a378d58555ef709c8d2ee574345dad
khamassa <amberine.khamassi@edu.esiee.fr>
2021-11-20 15:53:39 +0100

    P2C2

---
+++ partie2/my-first-vue-cli-app/src/components/NavLink.vue
@@ -0,0 +1,17 @@
+// ./components/NavLink.vue
+<script>
+export default {
+	name: 'NavLink',
+	props: ['url', 'text'] // 2 attributs : url, text
+}
+</script>
+
+<template>
+	<a href="url">{{ text }}</a> 
+</template>
+
+<style>
+	a {
+		text-decoration: none;
+	}
+</style>
