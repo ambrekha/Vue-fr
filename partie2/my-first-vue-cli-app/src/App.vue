662406fad131458ad1ee5683c178ab816a3f9e48
khamassa <amberine.khamassi@edu.esiee.fr>
2021-11-20 20:41:32 +0100

    Nothing new

--- partie2/my-first-vue-cli-app/src/App.vue
+++ partie2/my-first-vue-cli-app/src/App.vue
@@ -1,14 +1,55 @@
-<script>
-//import NavLink from './components/NavLink.vue'
-import MenuItem from './components/MenuItem.vue'
+<template>
+	<div id="app" class="app">
+		<h1>{{ restaurantName }}</h1>
+		<p class="description">
+			Bienvenue dans notre café {{ restaurantName }}! Nous sommes réputés pour
+			notre pain et nos merveilleuses pâtisseries. Faites vous plaisir dès le
+			matin ou avec un goûter réconfortant. Mais attention, vous verrez qu'il
+			est difficile de s'arrêter.
+		</p>
 
+		<section class="menu">
+			<h2>Menu</h2>
+			<MenuItem
+				v-for="item in simpleMenu"
+				:addToShoppingCart="addToShoppingCart"
+				:name="item.name"
+				:image="item.image"
+				:quantity="item.quantity"
+				:inStock="item.inStock"
+				:key="item.name"
+			/>
+		</section>
+
+		<aside class="shopping-cart">
+			<h2>Panier d'achat : {{ shoppingCart }} articles</h2>
+		</aside>
+
+		<h2>Contactez nous</h2>
+		<p>Adresse : {{ address }}</p>
+		<p>Téléphone : {{ phone }}</p>
+		<p>Email : {{ email }}</p>
+		<p>Horaires :</p>
+		<ul>
+			<li>L-V: 06:00 à 16:00</li>
+			<li>Samedi: 07:00 à 14:00</li>
+			<li>Dimanche: 07:00 à 12:00</li>
+		</ul>
+		<footer class="footer">
+			<p>{{ copyright }}</p>
+		</footer>
+	</div>
+</template>
+
+<script>
+import MenuItem from "./components/MenuItem"
 export default {
-  name: 'App',
-  components: {
-    //NavLink
-    MenuItem,
-    data() {
-      return {
+	name: "App",
+	components: {
+		MenuItem
+	},
+	data() {
+		return {
 			address: "18 avenue du Beurre, Paris, France",
 			email: "hello@cafewithavue.bakery",
 			phone: "01 88 88 88 88",
@@ -56,52 +97,8 @@ export default {
 			this.shoppingCart += amount
 		}
 	}
-  }
 }
 </script>
-
-<template>
-<div id="app" class="app">
-		<h1>{{ restaurantName }}</h1>
-		<p class="description">
-			Bienvenue dans notre café {{ restaurantName }}! Nous sommes réputés pour
-			notre pain et nos merveilleuses pâtisseries. Faites vous plaisir dès le
-			matin ou avec un goûter réconfortant. Mais attention, vous verrez qu'il
-			est difficile de s'arrêter.
-		</p>
-
-		<section class="menu">
-			<h2>Menu</h2>
-			<MenuItem
-				v-for="item in simpleMenu"
-				:addToShoppingCart="addToShoppingCart"
-				:name="item.name"
-				:image="item.image"
-				:quantity="item.quantity"
-				:inStock="item.inStock"
-				:key="item.name"
-			/>
-		</section>
-
-		<aside class="shopping-cart">
-			<h2>Panier d'achat : {{ shoppingCart }} articles</h2>
-		</aside>
-
-		<h2>Contactez nous</h2>
-		<p>Adresse : {{ address }}</p>
-		<p>Téléphone : {{ phone }}</p>
-		<p>Email : {{ email }}</p>
-		<p>Horaires :</p>
-		<ul>
-			<li>L-V: 06:00 à 16:00</li>
-			<li>Samedi: 07:00 à 14:00</li>
-			<li>Dimanche: 07:00 à 12:00</li>
-		</ul>
-		<footer class="footer">
-			<p>{{ copyright }}</p>
-		</footer>
-	</div>
-</template>
 
 <style lang="scss">
 #app {
